2943b32fef0889f8a1106965144557b37ee873f3
Mariell Hoversholm <mariell@mardroemmar.dev>
2024-06-02 10:11:56 +0200

    feat: use zsh-nix-shell

--- users/mariell/apps/zsh.nix
+++ users/mariell/apps/zsh.nix
@@ -15,6 +15,18 @@
     oh-my-zsh.enable = true;
     oh-my-zsh.theme = "robbyrussell";
     oh-my-zsh.plugins = [ "git" "1password" "fzf" ];
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
     enableCompletion = true;
     autosuggestion.enable = true;
 
