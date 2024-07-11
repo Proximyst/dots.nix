0b5a1df1c29f49c1c472b903ed254edc114764c2
Mariell Hoversholm <mariell@mardroemmar.dev>
2024-07-11 12:20:11 +0200

    feat: configure sddm a bit more

--- machines/common/display.nix
+++ machines/common/display.nix
@@ -29,10 +29,15 @@
       autoNumlock = true;
       package = pkgs.kdePackages.sddm;
       theme = "catppuccin-macchiato";
+      settings = {
+        Theme.CursorTheme = "catppuccin-macchiato-dark-cursors";
+      };
     };
   };
   services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 3440x1440 --rate 144 --primary
     ${pkgs.xorg.xrdb}/bin/xrdb -merge ${./Xresources}
+    ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr
   '';
   programs.dconf.enable = true;
 }
